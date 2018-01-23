e12163f6e75fd5a1e0161c4bb4d20e5e0fda55df
Hennadii <vankert353@gmail.com>
2018-01-23 22:07:54 +0200

    Сверстана секция Меню. Написан скрипт для аккордеона данной секции

--- work/js/accordeon.js
+++ work/js/accordeon.js
@@ -1,14 +1,45 @@
-var item = document.getElementsByClassName('accordeon__item');
+var itemV = document.getElementsByClassName('accordeon__item');
+var itemH = document.getElementsByClassName('horizon-accordeon__item');
 
-for (let i = 0; i < item.length; i++) {
-  item[i].addEventListener('click', function(el) {
+
+
+var verticalAccordeon = function() {
+    for (let i = 0; i < itemV.length; i++) {
+  itemV[i].addEventListener('click', function(el) {
     
     el.currentTarget.classList.toggle('accordeon__item--active');
     
-    for (let i = 0; i < item.length; i++) {
-      if (item[i] !== el.currentTarget && item[i].className === "accordeon__item accordeon__item--active") {
-        item[i].classList.remove('accordeon__item--active');
+    for (let i = 0; i < itemV.length; i++) {
+      if (itemV[i] !== el.currentTarget && itemV[i].className === "accordeon__item accordeon__item--active") {
+        itemV[i].classList.remove('accordeon__item--active');
       }
     }
   });
 };
+}
+
+
+var horizonAccordeon = function() {
+    for (let i = 0; i < itemH.length; i++) {
+  itemH[i].addEventListener('click', function(el) {
+    
+    el.currentTarget.classList.toggle('horizon-accordeon__item--active');
+    
+    for (let i = 0; i < itemH.length; i++) {
+      if (itemH[i] !== el.currentTarget && itemH[i].className === "horizon-accordeon__item horizon-accordeon__item--active") {
+        itemH[i].classList.remove('horizon-accordeon__item--active');
+      }
+    }
+  });
+};
+}
+
+
+
+
+
+
+
+verticalAccordeon();
+
+horizonAccordeon();
