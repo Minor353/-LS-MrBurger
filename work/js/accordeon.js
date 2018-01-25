756faa95edaf1ffd37fb7a3ffc747b6403ab5ea8
Hennadii <vankert353@gmail.com>
2018-01-26 00:29:27 +0200

    Сверстана секция Заказ

--- work/js/accordeon.js
+++ work/js/accordeon.js
@@ -1,37 +1,38 @@
 var itemV = document.getElementsByClassName('accordeon__item');
 var itemH = document.getElementsByClassName('horizon-accordeon__item');
 
+                          
 
+var verticalAccordeon = function () {
 
-var verticalAccordeon = function() {
     for (let i = 0; i < itemV.length; i++) {
-  itemV[i].addEventListener('click', function(el) {
-    
-    el.currentTarget.classList.toggle('accordeon__item--active');
-    
-    for (let i = 0; i < itemV.length; i++) {
-      if (itemV[i] !== el.currentTarget && itemV[i].className === "accordeon__item accordeon__item--active") {
-        itemV[i].classList.remove('accordeon__item--active');
-      }
-    }
-  });
-};
+        itemV[i].addEventListener('click', function (el) {
+
+            el.currentTarget.classList.toggle('accordeon__item--active');
+
+            for (let i = 0; i < itemV.length; i++) {
+                if (itemV[i] !== el.currentTarget && itemV[i].className === "accordeon__item accordeon__item--active") {
+                    itemV[i].classList.remove('accordeon__item--active');
+                }
+            }
+        });
+    };
 }
 
 
-var horizonAccordeon = function() {
+var horizonAccordeon = function () {
     for (let i = 0; i < itemH.length; i++) {
-  itemH[i].addEventListener('click', function(el) {
-    
-    el.currentTarget.classList.toggle('horizon-accordeon__item--active');
-    
-    for (let i = 0; i < itemH.length; i++) {
-      if (itemH[i] !== el.currentTarget && itemH[i].className === "horizon-accordeon__item horizon-accordeon__item--active") {
-        itemH[i].classList.remove('horizon-accordeon__item--active');
-      }
-    }
-  });
-};
+        itemH[i].addEventListener('click', function (el) {
+
+            el.currentTarget.classList.toggle('horizon-accordeon__item--active');
+
+            for (let i = 0; i < itemH.length; i++) {
+                if (itemH[i] !== el.currentTarget && itemH[i].className === "horizon-accordeon__item horizon-accordeon__item--active") {
+                    itemH[i].classList.remove('horizon-accordeon__item--active');
+                }
+            }
+        });
+    };
 }
 
 
