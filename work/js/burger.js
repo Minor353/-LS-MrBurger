819720ff265f4383d013a520e8dcdc1e9543e9a7
Hennadii <vankert353@gmail.com>
2018-02-04 17:03:40 +0200

    Запрещен скролл стрницы при открытом меню

--- work/js/burger.js
+++ work/js/burger.js
@@ -2,7 +2,11 @@ const openButton = document.querySelector(".burger");
 
 const navPanel = document.querySelector(".nav-panel");
 
+const bodyModal = document.body;
+
 openButton.addEventListener('click', function(){
     openButton.classList.toggle("burger-active");
     navPanel.classList.toggle("nav-panel-active");
+    bodyModal.classList.toggle("body-modal-open");
+    
 });
