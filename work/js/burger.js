f6928b73ea08fbfd9ba5b7c961ca52be0e30dfa1
Hennadii <vankert353@gmail.com>
2018-02-07 00:49:27 +0200

    Внесены правки на замечания наставника. Реализовано закрытие меню и переход по ссылке на виициимобильной версии.

--- work/js/burger.js
+++ work/js/burger.js
@@ -2,11 +2,28 @@ const openButton = document.querySelector(".burger");
 
 const navPanel = document.querySelector(".nav-panel");
 
+const menuLink = document.querySelectorAll(".nav-panel__item");
+
 const bodyModal = document.body;
 
-openButton.addEventListener('click', function(){
+
+
+
+
+const mobileMenu = function() {
+    openButton.addEventListener('click', function(){
     openButton.classList.toggle("burger-active");
     navPanel.classList.toggle("nav-panel-active");
     bodyModal.classList.toggle("body-modal-open");
-    
 });
+
+for (let i = 0; i < menuLink.length; i++) {
+        menuLink[i].addEventListener('click', function(){
+        openButton.classList.toggle("burger-active");
+        navPanel.classList.toggle("nav-panel-active");
+        bodyModal.classList.toggle("body-modal-open");
+});
+}
+};
+
+mobileMenu();
